b3cf394d0fc91c384c98c474c23b46b039d7c99c
Fredon99 <fredvss@hotmail.com>
2025-06-25 22:46:33 -0300

    update: updating the code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 import express from "express";
-import db from "./config/dbConnect.js"
-import routes from "./routes/index.js"
+import dbConnect from "./config/dbConnect.js";
+import routes from "./routes/index.js";
 
-db.on("error", console.log.bind(console, 'Erro de conexão'))
-db.once("open", () => {
-  console.log('conexão com o banco feita com sucesso')
+const conexao = await dbConnect();
+
+conexao.on("error", (error) => {
+    console.error("Erro de conexao", error)
+})
+
+conexao.once("open", () => {
+    console.log("Conexao com o banco feita com sucesso.")
 })
 
 const app = express();
-app.use(express.json())
 routes(app);
 
-export default app
+
+export default app;
+
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}.ect5b.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`);
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}.woeupll.mongodb.net/?retryWrites=true&w=majority&appName=Alura`);
 
 let db = mongoose.connection;
 
